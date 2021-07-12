ba08686a6384097daeca63ac8a4b4b81994f4805
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-07-12 23:19:23 +0200

    feat: filter bike activity samples by surface type #11

--- lib/data_transformer.py
+++ lib/data_transformer.py
@@ -11,7 +11,6 @@ from pathlib import Path
 
 
 class DataTransformer:
-
     BIKE_ACTIVITY_MEASUREMENT_SPEED_LIMIT = 5
 
     def run(self, data_path, results_path, clean=False):
@@ -33,6 +32,7 @@ class DataTransformer:
 
                 csv_reader = csv.DictReader(csv_file)
                 bike_activity_measurement_speed_valid = True
+                bike_activity_measurement_surface_type = True
 
                 with open(os.path.join(results_path, file_name), "w") as out_file:
                     # Make results path
@@ -45,21 +45,26 @@ class DataTransformer:
                         bike_activity_uid = row["bike_activity_uid"]
                         bike_activity_sample_uid = row["bike_activity_sample_uid"]
                         bike_activity_measurement_speed = float(row["bike_activity_measurement_speed"])
+                        bike_activity_surface_type = row["bike_activity_surface_type"]
 
                         if bike_activity_measurement_speed * 3.6 < self.BIKE_ACTIVITY_MEASUREMENT_SPEED_LIMIT:
                             bike_activity_measurement_speed_valid = False
+
+                        if bike_activity_surface_type is not 'mixed':
+                            bike_activity_measurement_surface_type = False
 
                         bike_activity_measurement_accelerometer_x = float(row["bike_activity_measurement_accelerometer_x"])
                         bike_activity_measurement_accelerometer_y = float(row["bike_activity_measurement_accelerometer_y"])
                         bike_activity_measurement_accelerometer_z = float(row["bike_activity_measurement_accelerometer_z"])
                         bike_activity_measurement_accelerometer = math.sqrt((bike_activity_measurement_accelerometer_x ** 2
-                                                                            + bike_activity_measurement_accelerometer_y ** 2
-                                                                            + bike_activity_measurement_accelerometer_z ** 2) / 3)
+                                                                             + bike_activity_measurement_accelerometer_y ** 2
+                                                                             + bike_activity_measurement_accelerometer_z ** 2) / 3)
 
                         row.update({"bike_activity_measurement_accelerometer": bike_activity_measurement_accelerometer})
                         csv_writer.writerow(row)
 
-                if not bike_activity_measurement_speed_valid:
+                if not bike_activity_measurement_speed_valid \
+                        or not bike_activity_measurement_surface_type:
                     os.remove(os.path.join(results_path, file_name))
 
             if bike_activity_measurement_speed_valid:
