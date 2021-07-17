cc4f377b38181239039f334deec41756ac0259c2
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-07-17 02:42:12 +0200

    refactor: data transformer

--- lib/data_transformer.py
+++ lib/data_transformer.py
@@ -1,6 +1,19 @@
 import math
 
 
+def getAccelerometer(row):
+    """
+    Calculates root mean square of accelerometer value components
+    """
+
+    bike_activity_measurement_accelerometer_x = float(row["bike_activity_measurement_accelerometer_x"])
+    bike_activity_measurement_accelerometer_y = float(row["bike_activity_measurement_accelerometer_y"])
+    bike_activity_measurement_accelerometer_z = float(row["bike_activity_measurement_accelerometer_z"])
+    return math.sqrt((bike_activity_measurement_accelerometer_x ** 2
+                      + bike_activity_measurement_accelerometer_y ** 2
+                      + bike_activity_measurement_accelerometer_z ** 2) / 3)
+
+
 #
 # Main
 #
@@ -10,19 +23,7 @@ class DataTransformer:
 
     def run(self, dataframes):
         for name, dataframe in list(dataframes.items()):
-            dataframe["bike_activity_measurement_accelerometer"] = dataframe.apply(lambda row: self.getAccelerometer(row), axis=1)
+            dataframe["bike_activity_measurement_accelerometer"] = dataframe.apply(lambda row: getAccelerometer(row), axis=1)
 
         print("DataTransformer finished.")
         return dataframes
-
-    def getAccelerometer(self, row):
-        """
-        Calculates root mean square of accelerometer value components
-        """
-
-        bike_activity_measurement_accelerometer_x = float(row["bike_activity_measurement_accelerometer_x"])
-        bike_activity_measurement_accelerometer_y = float(row["bike_activity_measurement_accelerometer_y"])
-        bike_activity_measurement_accelerometer_z = float(row["bike_activity_measurement_accelerometer_z"])
-        return math.sqrt((bike_activity_measurement_accelerometer_x ** 2
-                          + bike_activity_measurement_accelerometer_y ** 2
-                          + bike_activity_measurement_accelerometer_z ** 2) / 3)
