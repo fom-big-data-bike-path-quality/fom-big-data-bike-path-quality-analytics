1fa77bbabc6e111098717595118b8fa94670fb52
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-07-28 00:05:09 +0200

    feat: convert numpy arrays into tensor data sets #13

--- lib/base_model/cnn_base_model_helper.py
+++ lib/base_model/cnn_base_model_helper.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from torch.utils.data import TensorDataset
 
 
 def create_array(dataframes):
@@ -18,6 +20,20 @@ def create_array(dataframes):
     return np.dstack(array).transpose(2, 1, 0)
 
 
+def create_dataset(array):
+    epochs_count = len(array)
+    return TensorDataset(
+        # 3D array with
+        # axis-0 = epoch
+        # axis-1 = features in a measurement (INPUT)
+        # axis-2 = measurements in an epoch
+        torch.tensor(data=array[:epochs_count, :-1]).float(),
+        # 1D array with
+        # axis-0 = TARGET of an epoch
+        torch.tensor(data=array[:epochs_count, -1, :][:, 0]).long()
+    )
+
+
 #
 # Main
 #
@@ -28,3 +44,7 @@ class CnnBaseModelHelper:
         # Create arrays
         train_array = create_array(train_dataframes)
         test_array = create_array(test_dataframes)
+
+        # Create data sets
+        train_dataset = create_dataset(train_array)
+        test_dataset = create_dataset(test_array)
