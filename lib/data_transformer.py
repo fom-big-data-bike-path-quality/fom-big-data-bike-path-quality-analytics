81f50f9988ae6148ac09f24e4dd349b0089d92dc
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-07-28 00:00:20 +0200

    feat: drop unused columns #13

--- lib/data_transformer.py
+++ lib/data_transformer.py
@@ -26,6 +26,20 @@ class DataTransformer:
         for name, dataframe in list(dataframes.items()):
             dataframe["bike_activity_measurement_accelerometer"] = dataframe.apply(lambda row: getAccelerometer(row), axis=1)
             dataframe["bike_activity_surface_type_label_encoded"] = le.fit_transform(dataframe["bike_activity_surface_type"])
+            dataframe.drop(["bike_activity_uid",
+                            "bike_activity_sample_uid",
+                            "bike_activity_measurement",
+                            "bike_activity_measurement_timestamp",
+                            "bike_activity_measurement_lon",
+                            "bike_activity_measurement_lat",
+                            "bike_activity_measurement_speed",
+                            "bike_activity_measurement_accelerometer_x",
+                            "bike_activity_measurement_accelerometer_y",
+                            "bike_activity_measurement_accelerometer_z",
+                            "bike_activity_phone_position",
+                            "bike_activity_bike_type",
+                            "bike_activity_surface_type",
+                            "bike_activity_smoothness_type"], axis=1, inplace=True)
 
         print("DataTransformer finished.")
         return dataframes
