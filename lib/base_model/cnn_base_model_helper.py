165e7f6e98c1bce8dfa55bbb90cad3acc814dcdf
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-07-29 08:07:04 +0200

    feat: add training loop #13

--- lib/base_model/cnn_base_model_helper.py
+++ lib/base_model/cnn_base_model_helper.py
@@ -2,7 +2,11 @@ import os
 
 import numpy as np
 import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
 from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
 
 # Make library available in path
 library_paths = [
@@ -63,21 +67,79 @@ def create_loader(dataset, batch_size=128, shuffle=False, num_workers=0):
 
 class CnnBaseModelHelper:
 
-    def run(self, train_dataframes, test_dataframes):
+    def run(self, train_dataframes, validation_dataframes, test_dataframes, learning_rate=0.001, n_epochs=3000):
         # Create arrays
         train_array = create_array(train_dataframes)
+        validation_array = create_array(validation_dataframes)
         test_array = create_array(test_dataframes)
 
         # Create data sets
         train_dataset = create_dataset(train_array)
+        validation_dataset = create_dataset(validation_array)
         test_dataset = create_dataset(test_array)
 
         # Create data loaders
         train_data_loader = create_loader(train_dataset, shuffle=True)
+        validation_data_loader = create_loader(validation_dataset, shuffle=True)
         test_data_loader = create_loader(test_dataset, shuffle=False)
 
         # Define classifier
         classifier = Classifier(
-            input_channels=train_array.shape[1],
+            input_channels=1,  # TODO Derive this value from data
+            # input_channels=train_array.shape[1],
             num_classes=18
         ).to(device)
+        criterion = nn.CrossEntropyLoss(reduction='sum')
+        optimizer = optim.Adam(classifier.parameters(), lr=learning_rate)
+
+        accuracy_max = 0
+        patience, trials = 500, 0
+        base = 1
+        step = 2
+        loss_history = []
+        accuracy_history = []
+
+        # Run training loop
+        progress_bar = tqdm(iterable=range(1, n_epochs + 1), unit='epochs', desc="Train model")
+        for epoch in progress_bar:
+
+            classifier.train()
+            epoch_loss = 0
+            for i, batch in enumerate(train_data_loader):
+                x_raw, y_batch = [t.to(device) for t in batch]
+                optimizer.zero_grad()
+                out = classifier(x_raw)
+                loss = criterion(out, y_batch)
+                epoch_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+
+            epoch_loss /= train_array.shape[0]
+            loss_history.append(epoch_loss)
+
+            classifier.eval()
+            correct, total = 0, 0
+            for batch in validation_data_loader:
+                input, target = [t.to(device) for t in batch]
+                output = classifier(input)
+                preds = F.log_softmax(output, dim=1).argmax(dim=1)
+                total += target.size(0)
+                correct += (preds == target).sum().item()
+
+            accuracy = correct / total
+            accuracy_history.append(accuracy)
+
+            if epoch % base == 0:
+                print("Epoch " + str(epoch) + " loss " + str(round(epoch_loss, 4)) + " accuracy " + str(round(accuracy, 2)))
+                base *= step
+
+            # Check if accuracy increased
+            if accuracy > accuracy_max:
+                trials = 0
+                accuracy_max = accuracy
+                # torch.save(classifier.state_dict(), 'best.pth')
+            else:
+                trials += 1
+                if trials >= patience:
+                    print("No further improvement after epoch " + str(epoch))
+                    break
