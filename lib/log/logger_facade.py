8561c60f5c7ee7f77393f43bb988170ec840abd5
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-08-01 19:59:55 +0200

    feat: add logger facade #18

---
+++ lib/log/logger_facade.py
@@ -0,0 +1,20 @@
+from console_logger import ConsoleLogger
+from file_logger import FileLogger
+from telegram_logger import TelegramLogger
+
+
+class LoggerFacade:
+
+    def __init__(self, results_path, console=False, file=False, telegram=False):
+        self.results_path = results_path
+        self.console = console
+        self.file = file
+        self.telegram = telegram
+
+    def log_line(self, message, console=None, file=None, telegram=None):
+        if console or (console == None and self.console):
+            ConsoleLogger().log_line(message)
+        if file or (file == None and self.file):
+            FileLogger().log_line(self.results_path, message)
+        if telegram or (telegram == None and self.telegram):
+            TelegramLogger().log_line(message)
