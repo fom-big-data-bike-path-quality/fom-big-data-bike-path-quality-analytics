08d15dbf980db9a8ece19389f282d7c964f87dbe
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-09-17 01:34:50 +0200

    fix: manipulation of original dataframes dict

--- lib/data_preparation/data_filterer.py
+++ lib/data_preparation/data_filterer.py
@@ -13,35 +13,37 @@ class DataFilterer:
     @TrackingDecorator.track_time
     def run(self, logger, dataframes, quiet=False):
 
-        dataframes_count = len(dataframes.items())
+        copied_dataframes = dataframes.copy()
 
-        for name, dataframe in list(dataframes.items()):
+        dataframes_count = len(copied_dataframes.items())
+
+        for name, dataframe in list(copied_dataframes.items()):
 
             # Exclude dataframes which contain less than 500 measurements
             if len(dataframe) < 500:
                 if not quiet:
                     logger.log_line("✗️ Filtering out " + name + " (less than 500 measurements)")
-                dataframes.pop(name)
+                copied_dataframes.pop(name)
                 continue
 
             # Exclude dataframes which contain surface type 'mixed'
             if "mixed" in dataframe.bike_activity_surface_type.values:
                 if not quiet:
                     logger.log_line("✗️ Filtering out " + name + " (containing undefined surface type)")
-                dataframes.pop(name)
+                copied_dataframes.pop(name)
                 continue
 
             # Exclude dataframes which contain speeds slower than BIKE_ACTIVITY_MEASUREMENT_SPEED_LIMIT
             if (dataframe.bike_activity_measurement_speed.values * 3.6 < self.BIKE_ACTIVITY_MEASUREMENT_SPEED_LIMIT).any():
                 if not quiet:
                     logger.log_line("✗️ Filtering out " + name + " (containing slow measurements)")
-                dataframes.pop(name)
+                copied_dataframes.pop(name)
                 continue
 
             if not quiet:
                 logger.log_line("✓️ Keeping " + name)
 
-        dataframes_filtered_count = len(dataframes.items())
+        dataframes_filtered_count = len(copied_dataframes.items())
 
         if not quiet:
             class_name = self.__class__.__name__
@@ -50,4 +52,4 @@ class DataFilterer:
             logger.log_line(class_name + "." + function_name + " kept " + str(dataframes_filtered_count) + "/" + str(dataframes_count)
                             + " dataframes (" + str(round(dataframes_filtered_count / dataframes_count, 2) * 100) + "%)")
 
-        return dataframes
+        return copied_dataframes
