a50c0d4ff49efac2d1bf5bb0d131a0589aef5f43
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-09-22 00:51:08 +0200

    feat: add logger to tracking decorator

--- lib/tracking_decorator.py
+++ lib/tracking_decorator.py
@@ -5,13 +5,26 @@ class TrackingDecorator(object):
     def track_time(func):
 
         def wrap(self, *args, **kwargs):
+            if "logger" in kwargs:
+                logger = kwargs["logger"]
+            else:
+                logger = None
+
             start_time = datetime.now()
-            print("\n" + func.__qualname__ + " started")
+
+            if logger is not None:
+                logger.log_line("\n" + func.__qualname__ + " started")
+            else:
+                print("\n" + func.__qualname__ + " started")
 
             result = func(self, *args, **kwargs)
 
             time_elapsed = datetime.now() - start_time
-            print(func.__qualname__ + " finished in {}".format(time_elapsed))
+
+            if logger is not None:
+                logger.log_line(func.__qualname__ + " finished in {}".format(time_elapsed))
+            else:
+                print(func.__qualname__ + " finished in {}".format(time_elapsed))
 
             return result
 
