51c025d45a5d6e37fa40878c30d2742bc4f1d7c9
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-09-25 12:51:06 +0200

    fix: weird value error on logging

--- lib/base_model/cnn_base_model.py
+++ lib/base_model/cnn_base_model.py
@@ -547,23 +547,26 @@ class CnnBaseModel:
         )
 
         if not quiet:
-            logger.log_line("Cross-validation metrics")
-            logger.log_line(
-                "Mean accuracy " + str(round(np.mean(overall_validation_accuracy_history), 2)) + ", " +
-                " precision " + str(round(np.mean(overall_validation_precision_history), 2)) + ", " +
-                " recall " + str(round(np.mean(overall_validation_recall_history), 2)) + ", " +
-                " f1 score " + str(round(np.mean(overall_validation_f1_score_history), 2)) + ", " +
-                " cohen kappa score " + str(round(np.mean(overall_validation_cohen_kappa_score_history), 2)) + ", " +
-                " matthew correlation coefficient " + str(round(np.mean(overall_validation_matthew_correlation_coefficient_history), 2))
-            )
-            logger.log_line(
-                "Standard deviation " + str(round(np.std(overall_validation_accuracy_history), 2)) + ", " +
-                " precision " + str(round(np.std(overall_validation_precision_history), 2)) + ", " +
-                " recall " + str(round(np.std(overall_validation_recall_history), 2)) + ", " +
-                " f1 score " + str(round(np.std(overall_validation_f1_score_history), 2)) + ", " +
-                " cohen kappa score " + str(round(np.std(overall_validation_cohen_kappa_score_history), 2)) + ", " +
-                " matthew correlation coefficient " + str(round(np.std(overall_validation_matthew_correlation_coefficient_history), 2))
-            )
+            try:
+                logger.log_line("Cross-validation metrics")
+                logger.log_line(
+                    "Mean accuracy " + str(round(np.mean(overall_validation_accuracy_history), 2)) + ", " +
+                    " precision " + str(round(np.mean(overall_validation_precision_history), 2)) + ", " +
+                    " recall " + str(round(np.mean(overall_validation_recall_history), 2)) + ", " +
+                    " f1 score " + str(round(np.mean(overall_validation_f1_score_history), 2)) + ", " +
+                    " cohen kappa score " + str(round(np.mean(overall_validation_cohen_kappa_score_history), 2)) + ", " +
+                    " matthew correlation coefficient " + str(round(np.mean(overall_validation_matthew_correlation_coefficient_history), 2))
+                )
+                logger.log_line(
+                    "Standard deviation " + str(round(np.std(overall_validation_accuracy_history), 2)) + ", " +
+                    " precision " + str(round(np.std(overall_validation_precision_history), 2)) + ", " +
+                    " recall " + str(round(np.std(overall_validation_recall_history), 2)) + ", " +
+                    " f1 score " + str(round(np.std(overall_validation_f1_score_history), 2)) + ", " +
+                    " cohen kappa score " + str(round(np.std(overall_validation_cohen_kappa_score_history), 2)) + ", " +
+                    " matthew correlation coefficient " + str(round(np.std(overall_validation_matthew_correlation_coefficient_history), 2))
+                )
+            except:
+                pass
 
         return int(np.mean(overall_epochs))
 
